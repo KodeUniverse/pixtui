7711931df630126a0907b2e4614db9e5df763951
KodeUniverse <kodeuniverse@proton.me>
2026-05-26 03:51:25 -0400

    Added layout rendering with cards for palette, layers, sprites (not sure if ill include), and brush size

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 An interactive terminal app for creating, editing, and saving pixel art. Built with Rust and ratatui.
 
 ## Todo/Features in Roadmap:
-- Finish main functionality
-- Add layers
-- Add ability to export to PNG, create sprite sheet
-- Add method of creating a project file, and revisiting it to edit. Maybe bitmap format?
+- Finish main color editing functionality
+- Add cut and paste selection of pixels 
+- Add different brush sizes
+- Add layers functionality
+- Add ability to create sprite sheet
 
--- src/routes/editor/layout.rs
+++ src/routes/editor/layout.rs
@@ -51,32 +51,66 @@ impl Widget for &mut Editor {
         (&block).render(area, buf);
         let inner = block.inner(area);
 
-        let chunks = Layout::vertical([Constraint::Min(0), Constraint::Length(3)]).split(inner);
-        let canvas_area = chunks[0];
+        let outer_layout =
+            Layout::vertical([Constraint::Min(0), Constraint::Length(1)]).split(inner);
+        let inner_layout_def = Layout::horizontal([
+            Constraint::Percentage(20),
+            Constraint::Percentage(60),
+            Constraint::Percentage(20),
+        ]);
+        let inner_layout = inner_layout_def.split(inner);
+        let inner_layout_save = inner_layout_def.split(outer_layout[0]);
+
+        let mut default_render = |areas: &[Rect], buf: &mut Buffer| {
+            let left_panel_layout =
+                Layout::vertical([Constraint::Percentage(60), Constraint::Percentage(40)])
+                    .split(areas[0]);
+            let right_panel_layout =
+                Layout::vertical([Constraint::Percentage(50), Constraint::Percentage(50)])
+                    .split(areas[2]);
+
+            let left_panel = Block::default();
+            let palette_card = Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title_top(" Color Palette ")
+                .title_alignment(HorizontalAlignment::Center);
+            let brush_size_card = Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title_top(" Brush Size ")
+                .title_alignment(HorizontalAlignment::Center);
+            left_panel.render(areas[0], buf);
+            palette_card.render(left_panel_layout[0], buf);
+            brush_size_card.render(left_panel_layout[1], buf);
+
+            let right_panel = Block::default();
+            let layers_card = Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title_top(" Layers ")
+                .title_alignment(HorizontalAlignment::Center);
+            let sprite_card = Block::bordered()
+                .border_type(BorderType::Rounded)
+                .title_top(" Sprites ")
+                .title_alignment(HorizontalAlignment::Center);
+            sprite_card.render(right_panel_layout[0], buf);
+            layers_card.render(right_panel_layout[1], buf);
+            right_panel.render(areas[2], buf);
+
+            (&mut self.canvas).render(areas[1], buf);
+        };
+
         if !self.saving && !self.exporting {
-            (&mut self.canvas).render(inner, buf);
+            default_render(&inner_layout, buf);
         } else if self.saving {
-            let save_area = chunks[1];
-            let save_block = Block::default()
-                .borders(Borders::ALL)
-                .border_type(BorderType::Rounded);
-            (&save_block).render(save_area, buf);
+            let save_area = outer_layout[1];
+            let save_prompt = Paragraph::new(format!("Save project: {}.pxsc", self.input));
+            (&save_prompt).render(save_area, buf);
 
-            Paragraph::new(format!("Save project: {}.pxsc", self.input))
-                .render(save_block.inner(save_area), buf);
-
-            (&mut self.canvas).render(canvas_area, buf);
+            default_render(&inner_layout_save, buf);
         } else if self.exporting {
-            let export_area = chunks[1];
-            let export_block = Block::default()
-                .borders(Borders::ALL)
-                .border_type(BorderType::Rounded);
-            (&export_block).render(export_area, buf);
-
-            Paragraph::new(format!("Export to PNG: {}.png", self.input))
-                .render(export_block.inner(export_area), buf);
-
-            (&mut self.canvas).render(canvas_area, buf);
+            let export_area = outer_layout[1];
+            let export_text = Paragraph::new(format!("Export to PNG: {}.png", self.input));
+            (&export_text).render(export_area, buf);
+            default_render(&inner_layout_save, buf);
         }
     }
 }
